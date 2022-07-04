752ca09360e8c776f5e6750cb86b08ce6d485e68
Narine Georgyan <narinegeorgyan88@gmail.com>
2022-07-04 10:10:10 +0400

    change spaces

--- homework2/Weekly Salary/weeklySalary.js
+++ homework2/Weekly Salary/weeklySalary.js
@@ -1,30 +1,28 @@
-function weeklySalary(arr){
-    const TIME = 8;
-    let salary = 0;
-    const TIME_PRICE = 10;
-    const OVERTIME_PRICE = 15;
+function weeklySalary(arr) {
+  const TIME = 8;
+  let salary = 0;
+  const TIME_PRICE = 10;
+  const OVERTIME_PRICE = 15;
 
-    for([key, value] of arr.entries()){
-        if(value > 0){
-            let overTime = 0
-            if(value < TIME){
-                salary+=value * TIME_PRICE;
-            }
-            else{
-                overTime = value-TIME;
-            }
-           
-           if(value>=TIME){
-            salary+=(TIME * TIME_PRICE + overTime * OVERTIME_PRICE );
-           }
-           if(key == "5" && value!= 0 || key =="6" & value!=0 ){
-            salary+=(TIME * TIME_PRICE + overTime * OVERTIME_PRICE );
-           }
-           
-        }
-        }
-  
+  for ([key, value] of arr.entries()) {
+    if (value > 0) {
+      let overTime = 0;
+      if (value < TIME) {
+        salary += value * TIME_PRICE;
+      } else {
+        overTime = value - TIME;
+      }
+
+      if (value >= TIME) {
+        salary += TIME * TIME_PRICE + overTime * OVERTIME_PRICE;
+      }
+      if ((key == "5" && value != 0) || (key == "6") & (value != 0)) {
+        salary += TIME * TIME_PRICE + overTime * OVERTIME_PRICE;
+      }
+    }
+  }
+
   return salary;
 }
- const result = weeklySalary([0, 0, 0, 0, 0, 12, 0]);
- console.log(result)
+const result = weeklySalary([0, 0, 0, 0, 0, 12, 0]);
+console.log(result);
--- homework2/rgbToHex/rgbToHex.js
+++ homework2/rgbToHex/rgbToHex.js
@@ -1,25 +1,42 @@
-function rgbToHex(rgb){
-    const arrHex = ["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"]
-    rgb = rgb.split("rgb(").join("");
-    rgb = rgb.split(")").join("");
-    rgb = rgb.split(",");
-    rgb= rgb.reduce((agg,num)=>{
-        const mn = num%16;
-        const qan = parseInt(num/16);
- 
-        agg.push(qan, mn);
-     
-       return agg
-     },[])
-     .map(val=>{
-        for([key, value] of arrHex.entries()){
-            if(val===key){
-                return value;
-            }
-         }
-     }).join("");
-   return "#"+rgb
+function rgbToHex(rgb) {
+  const arrHex = [
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+  ];
+  rgb = rgb.split("rgb(").join("");
+  rgb = rgb.split(")").join("");
+  rgb = rgb.split(",");
+  rgb = rgb
+    .reduce((agg, num) => {
+      const mn = num % 16;
+      const qan = parseInt(num / 16);
 
+      agg.push(qan, mn);
+
+      return agg;
+    }, [])
+    .map((val) => {
+      for ([key, value] of arrHex.entries()) {
+        if (val === key) {
+          return value;
+        }
+      }
+    })
+    .join("");
+  return "#" + rgb;
 }
 console.log(rgbToHex("rgb(0, 128, 192)"));
-
